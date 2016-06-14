1488bc259a3797602240fb26ee65af021df5e2c4
mauricio-figueroa <mauricio.figueroa@despegar.com>
2016-06-14 12:09:14 -0300

    prueba boluda de structs

---
+++ prueba structs/pruebaStructs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+struct coordenada {
+   int x;
+   int y;
+  };
+  
+  
+  
+int main(){
+	
+	struct coordenada c1;
+	struct coordenada* r=&c1 ;
+
+	r->x=33;
+	printf("Ingrese la posicion en x: ");
+	scanf("%i",&r->x);
+	
+	
+	
+	printf("La coordenada en x es %i",c1.x);
+	
+	return 0;
+	
+	}
