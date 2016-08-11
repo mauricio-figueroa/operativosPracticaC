763d6f69e9b0ea2c7cbba863959c9990a8746070
Mauricio Figueroa <mauricio.figueroa@despegar.com>
2016-08-11 10:41:43 -0300

    d

--- Punterooos/src/Punterooos.c
+++ Punterooos/src/Punterooos.c
@@ -11,52 +11,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mostrarString(char[],int);
-void mostrarStringPuntero(char[],int);
-
+void mostrarString(char[], int);
+void sumarNumeros(int*, int*);
 
 int main(void) {
 
-
 	char string[25];
-	string[0]='M';
-	string[1]='A';
-	string[2]='U';
-	string[3]='R';
-	string[4]='Y';
+	string[0] = 'M';
+	string[1] = 'A';
+	string[2] = 'U';
+	string[3] = 'R';
+	string[4] = 'Y';
 
-	char *apuntadorString;
-	*apuntadorString=&string;
+	//int *num1 = malloc(sizeof(int));
+	//int *num2 = malloc(sizeof(int));
 
-	mostrarStringPuntero(apuntadorString,5);
+	//*num1 = 4;
+	//*num2 = 13;
 
+	//sumarNumeros(num1, num2);
 
-	mostrarString(string,5);
+	//mostrarString(string, 5);
 
+	int vector[5];
+
+	llenarVector(vector, 5);
 
 	return 0;
 
 }
+void llenarVector(int vector[], int cantPosiciones) {
+	int i;
+	int num = 0;
+	//int *p = malloc(sizeof(int));
+	//*p = num;
+	for (i = 0; i < cantPosiciones; i++) {
+		printf("Ingrese un numero: ");
+		scanf("%i", &num);
+		vector[i]=num;
+	}
+	int r;
+	int suma=0;
+	for (r = 0; r < cantPosiciones; r++) {
+		suma+=vector[r];
+		printf("Resultado=%i ",suma);
 
-
-void mostrarStringPuntero(char* punteroString[],int cantLetras){
-
-	int i=0;
-	for(i=0;i<cantLetras+1;i++){
-		printf("%c",*punteroString[i]);
 	}
 
 
 }
 
+void sumarNumeros(int *num1, int*num2) {
+	int resultado;
+	resultado = *num1 + *num2;
+	printf("resultado %i\n", resultado);
 
-void mostrarString(char palabra[],int cantLetras){
-	int i=0;
-
-	for(i=0;i<cantLetras+1;i++){
-		printf("%c",palabra[i]);
-	}
 }
 
+void mostrarString(char palabra[], int cantLetras) {
+	int i = 0;
 
+	for (i = 0; i < cantLetras + 1; i++) {
+		printf("%c", palabra[i]);
+	}
+}
 
